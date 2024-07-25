67970f254caab7f698afa95a81b050674858a6e3
idohu <idoh@armosec.io>
2024-07-25 20:08:48 +0300

    change objects formats
    Signed-off-by: idohu <idoh@armosec.io>

--- types/cloud_credentials.go
+++ types/cloud_credentials.go
@@ -6,39 +6,44 @@ import (
 	"github.com/armosec/armoapi-go/armotypes"
 )
 
-type AwsCredentials struct {
-	EncryptedExternalKey string       `json:"encryptedExternalKey" bson:"encryptedExternalKey"`
-	EncryptedRoleARN     string       `json:"encryptedRoleARN" bson:"encryptedRoleARN"`
-	Regions              []string     `json:"regions" bson:"regions"`
-	Services             []AwsService `json:"services" bson:"services"`
-}
-
 type AwsService struct {
 	Name string `json:"name" bson:"name"`
 }
 
+type AwsCredentials struct {
+	EncryptedExternalId string       `json:"encryptedExternalKey,omitempty" bson:"encryptedExternalKey,omitempty"`
+	EncryptedRoleARN    string       `json:"encryptedRoleARN,omitempty" bson:"encryptedRoleARN,omitempty"`
+	Regions             []string     `json:"regions,omitempty" bson:"regions,omitempty"`
+	Services            []AwsService `json:"services,omitempty" bson:"services,omitempty"`
+}
+
 type AzureCredentials struct {
-	EncryptedClientID     string `json:"encryptedClientID" bson:"encryptedClientID"`
-	EncryptedClientSecret string `json:"encryptedClientSecret" bson:"encryptedClientSecret"`
-	EncryptedTenantID     string `json:"encryptedTenantID" bson:"encryptedTenantID"`
-	SubscriptionID        string `json:"subscriptionID" bson:"subscriptionID"`
+	EncryptedTenantID       string `json:"encryptedTenantID,omitempty" bson:"encryptedTenantID,omitempty"`
+	EncryptedSubscriptionID string `json:"encryptedSubscriptionID,omitempty" bson:"encryptedSubscriptionID,omitempty"`
+}
+
+type GcpCredentials struct {
+	EncryptedPrincipalID string `json:"encryptedPrincipalID,omitempty" bson:"encryptedPrincipalID,omitempty"`
 }
 
 type Credentials struct {
-	AwsCredentials   `json:",inline" bson:"awsCredentials,omitempty"`
-	AzureCredentials `json:",inline" bson:"azureCredentials,omitempty"`
+	AwsCredentials   `json:",inline,omitempty" bson:"awsCredentials,omitempty"`
+	AzureCredentials `json:",inline,omitempty" bson:"azureCredentials,omitempty"`
+	GcpCredentials   `json:",inline,omitempty" bson:"gcpCredentials,omitempty"`
 }
 
 type CloudCredentials struct {
 	armotypes.PortalBase `json:",inline" bson:"inline"`
+	UpdatedBy            string      `json:"updatedBy" bson:"updatedBy"`
 	Enabled              bool        `json:"enabled" bson:"enabled"`
 	CreationTime         string      `json:"creationTime" bson:"creationTime"`
 	Provider             string      `json:"provider" bson:"provider"`
 	Credentials          Credentials `json:"credentials" bson:"credentials"`
+	AccountID            string      `json:"accountID" bson:"accountID"`
 }
 
 func (cc *CloudCredentials) GetReadOnlyFields() []string {
-	return clusterReadOnlyFields
+	return CloudCredentialsReadOnlyFields
 }
 func (cc *CloudCredentials) InitNew() {
 	cc.CreationTime = time.Now().UTC().Format(time.RFC3339)
@@ -57,3 +62,5 @@ func (cc *CloudCredentials) GetCreationTime() *time.Time {
 	}
 	return &creationTime
 }
+
+var CloudCredentialsReadOnlyFields = append([]string{"Provider"}, commonReadOnlyFieldsAllowRename...)
